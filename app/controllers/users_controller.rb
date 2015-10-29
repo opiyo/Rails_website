3da26b19541e001623f7f3d9c35ecaa58d8fcfb2
yuji <yuji.wa0507@gmail.com>
2015-10-29 17:56:12 +0900

    20151029 end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+	before_action :logged_in_user, only: [:edit, :update]
+	before_action :correct_user, only: [:edit, :update]
+	
 	def new
 		@user = User.new
 	end
@@ -19,9 +22,36 @@ class UsersController < ApplicationController
 		#debugger
 	end
 	
+	def edit
+		@user = User.find(params[:id])
+	end
+	
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+			flash[:success] = "更新完了！"
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
+	
 	private
 		def user_params
 			params.require(:user).permit(:name, :email, :password,
 											:password_confirmation)
+		end
+		
+		def logged_in_user
+			unless logged_in?
+				store_location
+				flash[:danger] = "ログインして下さい"
+				redirect_to login_url
+			end
+		end
+		
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_url) unless current_user?(@user)
 		end
 end
