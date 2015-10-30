8353f9f5f79cef3a1fd3f59e4bea412ca9e5c61a
yuji <yuji.wa0507@gmail.com>
2015-10-30 10:07:50 +0900

    Finish user edit, update, index actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,10 @@ class UsersController < ApplicationController
 			render 'new'
 		end
 	end
+	
+	def index
+		@users = User.paginate(page: params[:page], :per_page => 5)
+	end
 	  
 	def show
 		@user = User.find(params[:id])
